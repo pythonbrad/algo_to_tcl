0fecaa213661168323b51c362f1ee4c41fc49c4f
fomegne <fomegnemeudje@outlook.com>
2020-03-16 17:43:07 +0800

    update

--- test.sh
+++ test.sh
@@ -6,6 +6,6 @@ tclsh test.tcl
 for code in battery.agl table.agl test3.agl code.agl plus_or_menos.agl test2.agl test.agl test4.agl matrice.agl schtroumpf.agl factoriel.agl perimetre.agl;
 	do
 	echo "Test $code";
-	tclsh algo.tcl $code;
+	tclsh algo.tcl exemples/$code;
 	tclsh out.tcl;
 done
